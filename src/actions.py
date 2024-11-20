419920f47a3f616bb3ffcf69a8db5e0efef6e7e7
NicolasB <nicolas.blin@estaca.eu>
2024-11-20 09:25:23 +0100

    sphinx configuration for submodules + updated comments in actions.py for docstring type documentation (mandatory for autogeneration with sphinx). It works. To do : docstring comments on other python files

--- src/actions.py
+++ src/actions.py
@@ -1,36 +1,56 @@
 from utilities import *
 
-#reveal the case that is selected
-def reveal_case(row_selection, column_selection, number_of_rows, number_of_columns,board, list_of_mines, cases_revealed):
-        #INPUTS
-            #row_selection : selected row by the user
-            #column_selection : selected column by the user
-            #board : list of list (2D) containing integers with the number of mine around each case
-            #list of mines : list of positions (row,col) of the mines
-            #list_of_revealed_cases : list of the positions (row,col) of revealed cases
-        #OUTPUTS
-            #integer : 1 if the case contain a mine, 0 if it doesnt
 
+def reveal_case(row_selection, column_selection, number_of_rows, number_of_columns,board, list_of_mines, cases_revealed) -> bool:
+    """
+    Reveal the case that is selected.
 
-        #check if the revealed case contain a mine
-        if(is_there_a_mine(row_selection,column_selection,list_of_mines)):
-            #If the revealed case is a mine return 1
-            return 1
+    Parameters
+    ----------
+    row_selection : int
+        Selected row by the user.
+    column_selection : int
+        Selected column by the user.
+    number_of_rows : int
+        Total number of rows in the board.
+    number_of_columns : int
+        Total number of columns in the board.
+    board : list of list of int
+        2D list containing integers with the number of mines around each case.
+    list_of_mines : list of tuple of int
+        List of positions (row, col) of the mines.
+    cases_revealed : list of tuple of int
+        List of the positions (row, col) of revealed cases.
+
+    Returns
+    -------
+    bool
+        True if the case contains a mine, False if it doesn't.
+    """
+
+    #check if the revealed case contain a mine
+    if(is_there_a_mine(row_selection,column_selection,list_of_mines)):
+        #If the revealed case is a mine return 1
+        return 1
+    else:
+        #If the case is not a mine and the integer value stored in the board at this position is 0, reveals all the group of case that also contain a 0, else, just add the case to the list of revealed cases
+        if(board[row_selection][column_selection]==0):
+            reveal_neighbor_0_cases(row_selection, column_selection,number_of_rows, number_of_columns, board, cases_revealed)
+
         else:
-            #If the case is not a mine and the integer value stored in the board at this position is 0, reveals all the group of case that also contain a 0, else, just add the case to the list of revealed cases
-            if(board[row_selection][column_selection]==0):
-                reveal_neighbor_0_cases(row_selection, column_selection,number_of_rows, number_of_columns, board, cases_revealed)
+            cases_revealed.append((row_selection,column_selection))
+        #If the revealed case is not a mine return 0
+    return 0
 
-            else:
-                cases_revealed.append((row_selection,column_selection))
-            #If the revealed case is not a mine return 0
-            return 0
-
-
-#The user select to annotate or select a case to reveal
 def action_selection():
-    #OUTPUTS
-        #select_or_annotate : user selection, integer, 0 to reveal a case, 1 to annotate a case
+    """
+    The user selects to annotate or select a case to reveal.
+
+    Returns
+    -------
+    int
+        User selection, 0 to reveal a case, 1 to annotate a case.
+    """
 
     select_or_annotate = -1
     #while the input is not 0 or 1
@@ -41,16 +61,22 @@ def action_selection():
     return select_or_annotate
 
 
-#The user select a case
 def case_selection(number_of_rows, number_of_columns):
-    #INPUTS
-        #number_of_rows : number of rows of the board, integer
-        #number_of_columns : number of columns of the board, integer
-    #OUTPUTS
-        #row_selection : the row number selected by the user, integer
-        #col_selection : the column number selected by the user, integer
+    """
+    The user selects a case.
 
-    #The user select a row number while it is valid
+    Parameters
+    ----------
+    number_of_rows : int
+        Number of rows of the board.
+    number_of_columns : int
+        Number of columns of the board.
+
+    Returns
+    -------
+    tuple
+        The row number and column number selected by the user.
+    """
     row_selection = -1
     while not (row_selection>=0 and row_selection<number_of_rows):
         print("Select the row between 0 and " + str(number_of_rows-1))
