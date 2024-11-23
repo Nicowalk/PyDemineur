810bfaaf2edf5548a5321508c62ca931184b8b4a
NicolasB <nicolas.blin@estaca.eu>
2024-11-23 17:03:19 +0100

    added if __name__==__main__ in the main file to prevent execution when building and few changes to improve pylint score

--- src/pydemineur.py
+++ src/pydemineur.py
@@ -1,75 +1,80 @@
-"""Main file that contain the game loop"""
+"""Main file that contains the game loop for Minesweeper."""
 from board import create_board, print_board, print_board_in_game
 from actions import action_selection, case_selection, reveal_case
 from utilities import is_already_revealed, is_end_of_game
 
-# Size of the board
-NUMBER_OF_ROWS = 4
-NUMBER_OF_COLUMNS = 4
+def main():
+    """
+    Main function to run the Minesweeper game loop.
+    """
+    # Size of the board
+    number_of_rows = 10
+    number_of_columns = 10
 
-# Create the game board
-board, list_of_mines = create_board(NUMBER_OF_ROWS, NUMBER_OF_COLUMNS)
+    # Create the game board
+    board, list_of_mines = create_board(number_of_rows, number_of_columns)
 
-print_board(board, list_of_mines)
-# Game data : revealed cases and annotated cases (each case is either : not revealed or revealed;
-# An annotated case is always a not revealed case)
-revealed_cases = []
-annotated_cases = []
+    print_board(board, list_of_mines)
 
-# Condition of the end of game. True when a mine is clicked OR all the mines are
-GAME_FINISHED = False
+    # Game data: revealed cases and annotated cases
+    revealed_cases = []
+    annotated_cases = []
 
-# GAME LOOP
-while not GAME_FINISHED:
+    # Condition of the end of the game
+    game_finished = False
 
-    # Choose between select a case or annotate a case
-    ACTION_CHOICE = action_selection()
+    # GAME LOOP
+    while not game_finished:
 
-    # Select the row and column of the board
-    row_selection, col_selection = case_selection(NUMBER_OF_ROWS, NUMBER_OF_COLUMNS)
+        # Choose between selecting a case or annotating a case
+        action_choice = action_selection()
 
-    CASE_TYPE = 0
+        # Select the row and column of the board
+        row_selection, col_selection = case_selection(number_of_rows, number_of_columns)
 
-    # Reveal a case
-    if ACTION_CHOICE == 0:
-        # check if the case was already revealed
-        if is_already_revealed(row_selection, col_selection, revealed_cases):
-            print("You already clicked this case")
-        else:
-            # return either 1 if the case was a mine or 0 if not
-            CASE_TYPE = reveal_case(
-                row_selection,
-                col_selection,
-                board,
-                list_of_mines,
-                revealed_cases,
-            )
+        case_type = 0
 
-    # Annotate a case
-    if ACTION_CHOICE == 1:
-        # Check if the case was not already revealed
-        if (row_selection, col_selection) in revealed_cases:
-            print("Case already revealed")
-        else:
-            # Check if the case was not already annotated
-            if (row_selection, col_selection) in annotated_cases:
-                print("Case already annotated")
+        # Reveal a case
+        if action_choice == 0:
+            # Check if the case was already revealed
+            if is_already_revealed(row_selection, col_selection, revealed_cases):
+                print("You already clicked this case.")
             else:
-                # Add the case position to the list of annotated cases
+                # Return 1 if the case was a mine, or 0 otherwise
+                case_type = reveal_case(
+                    row_selection,
+                    col_selection,
+                    board,
+                    list_of_mines,
+                    revealed_cases,
+                )
+
+        # Annotate a case
+        elif action_choice == 1:
+            # Check if the case is already revealed or annotated
+            if (row_selection, col_selection) in revealed_cases:
+                print("Case already revealed.")
+            elif (row_selection, col_selection) in annotated_cases:
+                print("Case already annotated.")
+            else:
+                # Add the case to the annotated cases list
                 annotated_cases.append((row_selection, col_selection))
 
-    # Show the game board updated
-    print_board_in_game(board, list_of_mines, revealed_cases, annotated_cases)
+        # Update and display the game board
+        print_board_in_game(board, list_of_mines, revealed_cases, annotated_cases)
 
-    # Check if the game is win or lose
-    if is_end_of_game(
-        NUMBER_OF_ROWS, NUMBER_OF_COLUMNS, len(list_of_mines), len(revealed_cases)
-    ):
-        print("WINNER")
-        GAME_FINISHED = True
+        # Check if the game is won or lost
+        if is_end_of_game(
+            number_of_rows, number_of_columns, len(list_of_mines), len(revealed_cases)
+        ):
+            print("WINNER")
+            game_finished = True
 
-    if CASE_TYPE == 1:
-        print("LOSER")
-        GAME_FINISHED = True
+        if case_type == 1:
+            print("LOSER")
+            game_finished = True
 
-print("GAME OVER")
+    print("GAME OVER")
+
+if __name__ == "__main__":
+    main()
